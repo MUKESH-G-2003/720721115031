b0c7b2de03ceac71b5396cb3185a2a98172bcc04
MUKESH-G-2003 <127624466+MUKESH-G-2003@users.noreply.github.com>
2024-05-08 12:37:48 +0530

    Add files via upload

---
+++ 08-05-2024/ex1.py
@@ -0,0 +1,17 @@
+def CommonPrefix(s: list[str]):
+    pref = s[0]
+    lenght = len(pref)
+
+    for s in s[1:]:
+        while pref != s[0:lenght]:
+            lenght -= 1
+            if lenght == 0:
+                return '""'
+
+            pref = pref[0:lenght]
+
+    return pref
+
+
+print(CommonPrefix(["flower", "flow", "flight"]))
+print(CommonPrefix(["dog", "racecar", "car"]))
