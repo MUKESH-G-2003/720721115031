3c30a07145edae7ae4209bb3b3ae4496c6d4969b
MUKESH-G-2003 <127624466+MUKESH-G-2003@users.noreply.github.com>
2024-05-09 12:26:29 +0530

    Add files via upload

---
+++ 09-05-2024/exp2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+        return l
+result = Solution()
+nums = [3,4,5,6,7,9]
+target = 5
+print(result.searchInsert(nums, target))
